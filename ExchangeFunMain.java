493beb3946549dd9fa06180de1e5173ea5428d70
sean-morris <smorris@ausdk12.org>
2022-09-16 14:23:39 -0700

    Update ExchangeFunMain.java

--- ExchangeFunMain.java
+++ ExchangeFunMain.java
@@ -3,14 +3,8 @@ class ExchangeFunMain {
       ExchangeFun et = new ExchangeFun();
       int x = 3;
       int w = 30;
-  
-      int temp = x;
-      x= w;
-      w= temp;
-      //et.exchange(x, w);
-      System.out.println("We want:");
-      System.out.println("x = 30 w = 3");
-      System.out.println("Did we get it?:");
-      System.out.println ("x = " + x + "  w = " + w);
+      System.out.println (x + " and " + w);
+      et.exchange(x, w);
+      System.out.println (x + " and " + w);
     }
   }
